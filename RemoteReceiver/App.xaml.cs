8e0c8f22a7046a35adcb0d1d359ba118468ac2ee
gaunts <simon.diradourian@gmail.com>
2018-12-05 16:40:48 +0100

    systray ok

--- RemoteReceiver/App.xaml.cs
+++ RemoteReceiver/App.xaml.cs
@@ -5,13 +5,47 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace RemoteReceiver
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        NotifyIcon nIcon = new NotifyIcon();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitIcon();
+            CreateContextMenu();
+            this.nIcon.Visible = true;
+            await Task.Delay(2000);
+            this.nIcon.BalloonTipText = "Nique ta mere";
+            PressTheSpacebar();
+        }
+
+        private void InitIcon()
+        {
+            this.nIcon.Icon = new System.Drawing.Icon(@"../../fuck.ico");
+            this.nIcon.Text = "Fuck you bitch";
+        }
+
+        private void CreateContextMenu()
+        {
+            this.nIcon.ContextMenu = new System.Windows.Forms.ContextMenu();
+            this.nIcon.ContextMenu.MenuItems.Add("Exit").Click += (s, c) => { nIcon.Dispose(); System.Windows.Application.Current.Shutdown(); };
+        }
+
+        private void PressTheSpacebar()
+        {
+            InputSimulator sim = new InputSimulator();
+            sim.Keyboard.KeyPress(VirtualKeyCode.VOLUME_UP);
+            this.nIcon.ShowBalloonTip(2000);
+        }
     }
 }
---
+++ RemoteReceiver/EventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteReceiver
+{
+    public class EventListener
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
