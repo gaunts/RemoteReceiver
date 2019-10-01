4376c97cb41941904494e4bdb67cb0b0fda86dee
gaunts <simon.diradourian@gmail.com>
2019-10-01 21:20:21 +0200

    added arduino code to repo

---
+++ arduino/IRReceiver/sketch_jan12b/sketch_jan12b.ino
@@ -0,0 +1,35 @@
+/*
+ * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+ * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
+
+#include <IRremote.h>
+
+int RECV_PIN = 2;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.setTimeout(5);
+  Serial.read();
+}
+
+void serialEvent(){
+  String test = Serial.readString();
+  if (test == "who are you bitch")
+    Serial.println("qkjweklqjwe87dahgdq9qwd");
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.print(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
